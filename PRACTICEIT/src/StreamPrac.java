e2fa4349fe342f8c9b3b5582f04d5ef53c88eeb8
sam <samuel13241@gmail.com>
2022-06-12 15:34:34 -0700

    Changes

---
+++ PRACTICEIT/src/StreamPrac.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamPrac {
+    public static void main(String[] args) {
+        String[] arr = new String []{"a", "b", "C"};
+        Stream<String> stream = Arrays.stream(arr);
+
+    }
+
+}
